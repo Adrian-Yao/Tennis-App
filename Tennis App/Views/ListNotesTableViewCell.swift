60550f25c40f767832edc9e5453a34395ae96aa9
Adrian-Yao <adrian.c.yao@gmail.com>
2017-08-08 10:35:43 -0400

    Almost done with UI

---
+++ Tennis App/Views/ListNotesTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ListNotesTableViewCell.swift
+//  Tennis App
+//
+//  Created by adyao20 on 8/7/17.
+//  Copyright © 2017 adyao20. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListNotesTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var nameButton: UILabel!
+    @IBOutlet weak var infoButton: UILabel!
+    @IBOutlet weak var levelButton: UILabel!
+    
+}
