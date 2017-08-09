0e222e4484c0ac708dee98f08cd9a240a0db9428
Adrian-Yao <adrian.c.yao@gmail.com>
2017-08-09 16:49:29 -0400

    fixed picker and info text view

--- Tennis App/Controllers/ProfileViewController.swift
+++ Tennis App/Controllers/ProfileViewController.swift
@@ -9,11 +9,53 @@
 import UIKit
 
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    var age: Int?
+    var gender: Bool?
+    var level: Float?
+    var country: String?
+    var city: String?
+    var phoneNumber: String?
+    var info: String?
+    
+    //PICKER VIEWS
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 0 {
+            return 83
+        } else if pickerView.tag == 1 {
+            return 14
+        } else {
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if pickerView.tag == 0 {
+            return String(row + 18) //age
+        } else if pickerView.tag == 1 {
+            return String(Double(row) / 2 ) //level
+        } else {
+            return ""
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        agePicker.delegate = self
+        agePicker.dataSource = self
+        levelPicker.delegate = self
+        levelPicker.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
+        self.hideKeyboardWhenTappedAround()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +63,7 @@ class ProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+   
 //OUTLET
     @IBOutlet weak var profilePicButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
@@ -33,24 +76,50 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var infoTextView: UITextView!
     @IBOutlet weak var matchButton: UIButton!
     
+    
     //ACTION
     
+    
     @IBAction func profileButtonTapped(_ sender: Any) {
+        
+        
+        
         print("Profile ICE CREAM")
     }
     
-    @IBAction func genderSegmentControlTapped(_ sender: Any) {
+    
+    @IBAction func genderSegmentedControlTapped(_ sender: Any) {
+        
         
         print("SEGMENT CONTROL ICE CREAM")
     }
     
     
     @IBAction func matchButtonTapped(_ sender: Any) {
-     
+        func create(_firUser:FIRUser, age: Int?, gender: Bool?, level: Float, country: String?, city: String?, phoneNumber: String?, info:String?) {
+            
+            
+            
+        }
         print("MATCH BUTTON ICE CREAM")
+
     }
+  
     
     
-    
+
 }
 
+//EXTENSIONS
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
